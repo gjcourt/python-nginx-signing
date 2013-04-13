d617d21c7646d1890727ad8ceb9e1e7ac5a19909
George Courtsunis <gjcourt@gmail.com>
2013-04-12 22:37:16 -0700

    Fix packaging

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ import nginx_signing
 from setuptools import setup
 
 setup(
-    name = 'nginx_signing',
-    version = nginx_signing.__version__,
-    author = "George Courtsunis",
-    author_email = "gjcourt@gmail.com",
-    description = "Signs urls to work with the nginx Secure Link module",
-    license = "MIT License",
-    keywords = "nginx signing secure_link",
-    url = "http://github.com/gjcourt/python-nginx-signer",
+    name='nginx_signing',
+    version=nginx_signing.__version__,
+    author="George Courtsunis",
+    author_email="gjcourt@gmail.com",
+    description="Signs urls to work with the nginx Secure Link module",
+    license="MIT License",
+    packages=['nginx_signing'],
+    keywords="nginx signing secure_link",
+    url="http://github.com/gjcourt/python-nginx-signer",
 )
